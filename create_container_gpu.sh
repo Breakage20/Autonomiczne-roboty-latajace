69d153229e0e4d6fcd936d25de8e6dd50f4671ac
Breakage20 <gaweckiblazej@gmail.com>
2022-12-17 23:56:59 +0100

    Added support for gpu and without gpu

--- create_container_gpu.sh
+++ create_container_gpu.sh
@@ -16,12 +16,15 @@ fi
 docker stop ARL || true && docker rm ARL || true
 
 docker run -it \
+    --gpus all \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --env="XAUTHORITY=$XAUTH" \
     --volume="$XAUTH:$XAUTH" \
     --volume "$PWD/src:/root/tello_ws/src" \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
     --privileged \
     --network=host \
     --name="ARL" \
