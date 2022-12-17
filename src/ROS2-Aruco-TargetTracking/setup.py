dfa7084592e7d817628985c8d00aed979dc4e4db
Breakage20 <gaweckiblazej@gmail.com>
2022-12-17 23:41:54 +0100

    Fixed package problem

---
+++ src/ROS2-Aruco-TargetTracking/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+from glob import glob
+from setuptools import setup
+
+package_name = 'camera_target_tracking'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Marco Lapolla',
+    maintainer_email='marco.lapolla5@gmail.com',
+    description='Aruco Target Tracking',
+    license='BSD',
+    entry_points={
+        'console_scripts': [
+        'aruco_detector = camera_target_tracking.aruco_detector:main',
+        'simulate_marker = camera_target_tracking.simulate_marker:main',
+        ],
+    },
+)
